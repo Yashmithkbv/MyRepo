a98ed94c0f04d1704f39c9116ca1ed9c571d2c56
Yashmith <yashkbv@gmail.com>
2025-03-27 15:12:58 +0530

    adding js changes

--- force-app/main/default/lwc/sampleComponentWithIssues/sampleComponentWithIssues.js
+++ force-app/main/default/lwc/sampleComponentWithIssues/sampleComponentWithIssues.js
@@ -9,4 +9,16 @@ export default class SampleComponentWithIssues extends LightningElement {
         // Issue 1: Console log left in production code
         console.log('Action handled in SonarTestComponent');
     }
+    
+    // Issue 3: Hardcoded credentials (Security issue)
+    getSensitiveData() {
+        const password = '12345'; // Hardcoded password
+        return password;
+    }
+
+    // Issue 4: Unreachable code
+    problematicFunction() {
+        return;
+        console.log('This code will never execute'); // Unreachable code
+    }
 }
